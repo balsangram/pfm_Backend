c2d22d3ff9b3cd97ee12fe5722ba1d7ec29b0241
sangram <balsangram1@gmail.com>
2025-08-26 19:35:58 +0530

    update

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 
 // CORS configuration for frontend
 const corsOptions = {
-    origin: ['http://localhost:5173', 'http://localhost:3000', 'http://localhost:3001'],
+    origin: ['http://localhost:5173', 'http://localhost:3000', 'http://localhost:3001', 'https://pfm.kods.app'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     // allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
@@ -36,7 +36,7 @@ app.use((req, res, next) => {
 
 // Example route
 app.get('/', (req, res) => {
-    res.send('Server is running! 22-08-2025 3.36pm');
+    res.send('Server is running! 26-08-2025 3.36pm');
 });
 
 app.get('/api/example', (req, res) => {
